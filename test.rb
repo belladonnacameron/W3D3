14889f3db7138b41a5788507d974e870603515b9
acew1zaRD <96218034+belladonnacameron@users.noreply.github.com>
2022-02-16 11:40:10 -0500

    Add files via upload

--- test.rb
+++ test.rb
@@ -1,3 +1,17 @@
-def 
- puts 1
-end 
+# def range (num) 
+#   narr = []
+#   (1..num).each do |x|
+#     narr << x   
+#   end 
+# return narr
+# end 
+
+# puts range(9)
+
+def range_recursive (num)
+  return [1] if num ==  1
+
+  range_recursive(num-1) + [num]
+end
+
+puts range_recursive (9)
