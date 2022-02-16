817b52b3ed77a90c200ead3d883b76b44acb00c9
acew1zaRD <96218034+belladonnacameron@users.noreply.github.com>
2022-02-16 17:23:58 -0500

    Add files via upload

--- test.rb
+++ test.rb
@@ -103,3 +103,94 @@ require 'byebug'
 # p fib_rec(6) 
 
 
+# def bsearch (arr, el)
+
+#   return nil if arr.length == 0
+
+#   mid_idx = arr.length / 2
+#   mid_item = arr[mid_idx]
+
+#   arr_1 = arr[0...mid_idx]
+#   arr_2 = arr[mid_idx+1...arr.length]
+  
+#   if el == mid_item 
+#     return mid_idx
+#   elsif el > mid_item 
+#     # p mid_item 
+#     next_search = bsearch(arr_2, el)
+#     return nil if next_search == nil
+#     return mid_idx + 1 + next_search
+#   else
+#     return bsearch(arr_1, el)
+#   end
+
+# end
+  
+# p bsearch([1, 2, 3], 1) # => 0
+# p bsearch([2, 3, 4, 5], 3) # => 1
+# p bsearch([2, 4, 6, 8, 10], 6) # => 2
+# p bsearch([1, 3, 4, 5, 9], 5) # => 3
+# p bsearch([1, 2, 3, 4, 5, 6], 6) # => 5
+# p bsearch([1, 2, 3, 4, 5, 6], 0) # => nil
+# p bsearch([1, 2, 3, 4, 5, 7], 6) # => nil
+
+def merge_sort(arr)
+    if arr.length == 1
+       return arr
+    end
+    mid_idx = arr.length / 2
+    mid_item = arr[mid_idx]
+    arr_1 = arr[0...mid_idx]
+    arr_2 = arr[mid_idx...arr.length]
+    sorted_arr = merge(merge_sort(arr_1), merge_sort(arr_2))
+end
+
+
+def merge(arr_1, arr_2)
+   narr = []
+   if arr_1.length > arr_2.length 
+      length = arr_2.length
+   else
+     length = arr_1.length
+   end
+   
+   while true
+    if arr_2.length == 0
+      narr += arr_1
+      break
+    elsif arr_1.length == 0
+      narr += arr_2
+      break
+    else
+      if arr_2[0] > arr_1[0]
+        smaller_item = arr_1.shift
+        narr << smaller_item
+      else
+        smaller_item = arr_2.shift
+        narr << smaller_item
+      end
+    end
+   end
+   narr
+end 
+
+arr1 = [4,5,6]
+arr2 = [8,9,10]
+
+p merge(arr1, arr2) #=> [2,4,5,6,8,9]
+
+# p merge_sort([2,5,4,7,9,3,8])
+
+
+
+# def subsets()
+
+
+# end
+
+# subsets([]) # => [[]]
+# subsets([1]) # => [[], [1]]
+# subsets([1, 2]) # => [[], [1], [2], [1, 2]]
+# subsets([1, 2, 3])
+# => [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
+
